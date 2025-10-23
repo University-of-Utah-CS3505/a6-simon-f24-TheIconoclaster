62d13066a720db355759b9a53ead18740512e073
TheIconoclaster <157529389+TheIconoclaster@users.noreply.github.com>
2025-10-22 19:18:56 -0600

    Initialized Model and variables

---
+++ simonmodel.cpp
@@ -0,0 +1,19 @@
+#include "simonmodel.h"
+
+SimonModel::SimonModel(QObject *parent)
+    : QObject{parent}
+{
+    currentScore = 0;
+    playersTurn = false;
+    speedFactor = 1;
+    timer.setInterval(1000);
+    connect(&timer, &QTimer::timeout, this, &SimonModel:: nextTurn);
+
+}
+
+void SimonModel::startGame(){
+
+
+
+}
+
---
+++ simonmodel.h
@@ -0,0 +1,35 @@
+#ifndef SIMONMODEL_H
+#define SIMONMODEL_H
+
+#include <QObject>
+#include <queue>
+#include <QTimer>
+
+class SimonModel : public QObject
+{
+    Q_OBJECT
+private:
+    enum Colors {RED, BLUE};
+    int currentScore;
+    bool playersTurn;
+    //TODO: int highScore;
+    std::vector<Colors> sequence;
+    std::queue<Colors> currentTurn;
+    float speedFactor;
+    int progress;
+    QTimer timer;
+
+public:
+    explicit SimonModel(QObject *parent = nullptr);
+
+signals:
+    void updateProgress();
+
+public slots:
+    void startGame();
+    void nextTurn();
+    void endGame();
+
+};
+
+#endif // SIMONMODEL_H
