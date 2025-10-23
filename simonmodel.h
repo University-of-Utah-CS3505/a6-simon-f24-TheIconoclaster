23b521ca2ef9becb350425939ceb0e7c28eaa692
TheIconoclaster <157529389+TheIconoclaster@users.noreply.github.com>
2025-10-23 17:04:22 -0600

    Updated model with Qtimer logic and queue for each game

--- simonmodel.h
+++ simonmodel.h
@@ -8,31 +8,57 @@
 class SimonModel : public QObject
 {
     Q_OBJECT
-private:
+
+public:
     enum Colors { RED = 0, BLUE = 1 };
+    explicit SimonModel(QObject *parent = nullptr);
+
+    int getCurrentScore() const { return currentScore; }
+    int getHighScore() const { return highScore; }
+    bool isPlayersTurn() const { return playersTurn; }
+
+signals:
+    void gameStarted();
+    void gameEnded(int finalScore);
+    void progressUpdated(int progress);
+    void colorToFlash(int colorIndex);
+    void turnStarted(int sequenceLength);
+    void scoreChanged(int score);
+    void highScoreChanged(int highScore);
+
+public slots:
+    void startGame();
+    void playerPressed(int colorIndex);
+
+private slots:
+    void processNextFlash();
+    void startPlayerTurn();
+
+private:
     int currentScore;
     bool playersTurn;
-    //TODO: int highScore;
+    int highScore;
+    bool gameActive;
     std::vector<Colors> sequence;       // Full sequence of colors
-    std::queue<Colors> currentTurn;     // Remaining flashes
+    std::queue<Colors> flashQueue;     // Remaining flashes
     int playerIndex = 0;                // position during turn
 
 
     float speedFactor;
-    int progress;
-    QTimer timer;
+    int playerInputIndex = 0;
+    QTimer flashTimer;
+    QTimer playerTurnTimer;
 
-public:
-    explicit SimonModel(QObject *parent = nullptr);
-
-signals:
-    void updateProgress();
-
-public slots:
-    void startGame();
     void nextTurn();
     void endGame();
-    void playerPressed(int colorIndex);
+    void addRandomColor();
+    void flashSequence();
+    void resetGameState();
+
+    static const int INITIAL_FLASH_DELAY = 1000;  // milliseconds
+    static const int MIN_FLASH_DELAY = 300;      // minimum speed
+    static const int SPEED_DECREMENT = 50;       // how much faster each turn
+
 };
 
 #endif // SIMONMODEL_H
