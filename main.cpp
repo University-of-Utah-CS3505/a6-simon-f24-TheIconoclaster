ff697cd679b09706b1a6bba82dfd17164df2b2d1
SinahiLeon <143039397+SinahiLeon@users.noreply.github.com>
2025-10-23 18:06:06 -0600

    Main window .h file from master
    Refactor main.cpp to include MainWindow class definition and SimonModel dependency.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
-#include "mainwindow.h"
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "simonmodel.h"
 
-#include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+#include <QMainWindow>
 
-    MainWindow w;
-    w.show();
-    return a.exec();
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
 }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(SimonModel& model, QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+    // To do - add signals when user presses start and what was pressed
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
